dbb17b88a5f69857508d469ae7428ec4f878e8cb
Daniel Zehe <daniel.zehe@tum-create.edu.sg>
2017-04-14 22:43:42 +0800

    - Added thumbnail calculation on upload - better port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const multer = require('multer')
 const fs = require('fs')
-
+const thumb = require('node-thumbnail').thumb;
 const app = express()
 app.set('views',__dirname + '/views')
 app.set('view engine','pug')
@@ -14,9 +14,14 @@ let api_router = express.Router();
 let web_router = express.Router();
 
 app.use('/boothpic',express.static(__dirname + '/uploads'));
+app.use('/thumbs',express.static(__dirname + '/thumbs'));
 app.use('/static',express.static(__dirname + '/static'));
 
-
+app.get('/',function(req,res){
+	fs.readdir(__dirname+'/uploads', (err,files)=>{
+		res.render('overview',{pics:files});
+	});
+})
 api_router.get('/',function(req,res){
 	res.status(200).send("all API Endpoints");
 });
@@ -36,7 +41,19 @@ api_router.post('/upload',  uploading.single('avatar'), function(req, res,next) 
 			console.log(err)
 		}
 		else{
-			res.sendStatus(200)
+
+			thumb({
+			  source: __dirname+'/uploads/'+req.file.originalname, // could be a filename: dest/path/image.jpg 
+			  destination: __dirname+'/thumbs/',
+			  concurrency: 2,
+			  overwrite: true,
+			  width: 800,
+			  suffix: ''
+			}, function(err2, stdout, stderr) {
+				// console.log([err,stdout,stderr]);
+			  	console.log('All done!');
+			  	res.sendStatus(200);
+			});
 		}
 	});
 })
@@ -59,6 +76,6 @@ app.use('/api',api_router);
 app.use('/web',web_router);
 
 
-app.listen(3333,function(){
-	console.log('listening on 3333');
+app.listen(64888,function(){
+	console.log('listening on 64888');
 });
